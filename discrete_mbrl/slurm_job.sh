ba8b5fd4ff3d1e79c2fcae9619b459c3edbfc1f5
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2024-12-11 11:52:25 -0700

    update slurm for a sweep id

--- discrete_mbrl/slurm_job.sh
+++ discrete_mbrl/slurm_job.sh
@@ -48,6 +48,8 @@ elif [[ "$1" == "vqvae_count_door_key" ]]; then
    python comet_sweep.py --sweep_id "new" --config "sweep_configs/comet_ml/model_free_experiments/delayed_vanilla_mf/vqvae_count_door_key.json"
 elif [[ "$1" == "vqvae_count_door_key_sweep" ]]; then
    python comet_sweep.py --sweep_id "new" --config "sweep_configs/comet_ml/model_free_experiments/delayed_vanilla_mf_sweep/vqvae_count_door_key.json"
+elif [[ "$1" == "sweep_id" ]]; then
+   python comet_sweep.py --sweep_id "$2"
 else
    echo "Not a valid sweep"
 fi
