e124de409354ba64741a65e139f93015dee4e9a4
Mahshid Rahmani Hanzaki <mahshidrahmani2001@gmail.com>
2025-02-23 09:49:53 -0700

    Fix args for wandb sweep

--- discrete_mbrl/scripts/slurm_job_visitation_wandb.sh
+++ discrete_mbrl/scripts/slurm_job_visitation_wandb.sh
@@ -30,19 +30,19 @@ git clone https://github.com/artemis79/discrete-representation-exploraion
 #Install uv 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 cd discrete-representation-exploraion/
-uv venv
-source .venv/bin/activate
 
-uv pip sync pyproject.toml
-uv add 'requests[socks]' 
+# Install packages
+uv venv $SLURM_TMPDIR/.venv --python 3.10
+source $SLURM_TMPDIR/.venv/bin/activate
+uv pip install -r pyproject.toml --cache-dir $SLURM_TMPDIR/uv/cache
+uv pip install requests[socks] 
 
 cd discrete_mbrl/model_free
 
 python train.py --wandb --count --ae_recon_loss --ae_er_train --log_pos --device "cuda" --log_freq 10000 --learning_rate 0.0003 --batch_size 256 \
                 --mf_steps 1000000 --embedding_dim 64 --env_max_steps 1000 --n_ae_updates 8 --ppo_batch_size 64 --ppo_value_coef 0.5 \
-                --ppo_iters 10 --ppo_clip 0.2 --rl_start_step 500000 --beta 0.1 --ae_model_type "vqvae" --filter_size 6 --codebook_size 256 \
-                --beta 0.01 --env_name "minigrid-door-key-stochastic" --stochastic "categorical"
-
+                --ppo_iters 10 --ppo_clip 0.2 --rl_start_step 500000 --ae_model_type "vqvae" --filter_size 6 --codebook_size 256 \
+                --beta 0.01 --env_name "minigrid-door-key-stochastic" --stochastic "categorical" --rm_reward
 
 wandb sync --sync-all
 
